81eca6f6fdbb83c7b7392d377410aa74a67c40f2
beortizz <beortizz@gmail.com>
2024-03-08 21:21:56 -0300

    add HolidayPlan model and migration

---
+++ app/Http/Controllers/HolidayPlanController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HolidayPlan;
+use App\Http\Requests\StoreHolidayPlanRequest;
+use App\Http\Requests\UpdateHolidayPlanRequest;
+
+class HolidayPlanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreHolidayPlanRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(HolidayPlan $holidayPlan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(HolidayPlan $holidayPlan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateHolidayPlanRequest $request, HolidayPlan $holidayPlan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(HolidayPlan $holidayPlan)
+    {
+        //
+    }
+}
